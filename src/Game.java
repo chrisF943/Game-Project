f81fc2e1c5895e4ac048281cce036e64982633bb
chris <chris.faris@icloud.com>
2023-09-18 19:16:40 -0400

    test

---
+++ src/Game.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.Random;
+
+public class Game {
+
+    private static final String GAME_STATE_FILE = "gameState.csv";
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        GameState gameState = readGameStateFromFile();
+
+        int randomNumber = random.nextInt(10);
+        int randomNumber2 = random.nextInt(10);
+        int totalRoll = randomNumber + randomNumber2;
+        gameState.setSpacesMoved(gameState.getSpacesMoved() + totalRoll);
+
+        if (gameState.getSpacesMoved() >= 50) {
+            System.out.println("Game over");
+            gameState.setSpacesMoved(0);
+            gameState.setCoinAmount(0);
+            gameState.setStarAmount(0);
+        }
+
+        System.out.println("You rolled: " + totalRoll);
+        System.out.println("Total spaces moved: " + gameState.getSpacesMoved());
+        System.out.println("You have " + gameState.getCoinAmount() + " coins");
+        System.out.println("You have " + gameState.getStarAmount() + " stars");
+
+        writeGameStateToFile(gameState);
+    }
+
+    public static GameState readGameStateFromFile() {
+        try (BufferedReader br = new BufferedReader(new FileReader(GAME_STATE_FILE))) {
+            String line = br.readLine();
+            if (line != null) {
+                String[] parts = line.split(",");
+                int spacesMoved = Integer.parseInt(parts[0]);
+                int coinAmount = Integer.parseInt(parts[1]);
+                int starAmount = Integer.parseInt(parts[2]);
+                return new GameState(spacesMoved, coinAmount, starAmount);
+            }
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("Error reading file: " + e);
+        }
+        return new GameState();
+    }
+
+    public static void writeGameStateToFile(GameState gameState) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(GAME_STATE_FILE))) {
+            String csvLine = String.format("%d,%d,%d",
+                    gameState.getSpacesMoved(), gameState.getCoinAmount(), gameState.getStarAmount());
+            bw.write(csvLine);
+        } catch (IOException e) {
+            System.out.println("Error writing file: " + e);
+        }
+    }
+}//end class
---
+++ src/GameState.java
@@ -0,0 +1,51 @@
+import java.io.Serializable;
+
+public class GameState implements Serializable {
+    private int spacesMoved;
+    private int coinAmount;
+    private int starAmount;
+
+
+    public GameState(){
+
+    }
+
+    public GameState (int spacesMoved, int coinAmount, int starAmount){
+        this.spacesMoved = spacesMoved;
+        this.coinAmount = coinAmount;
+        this.starAmount = starAmount;
+    }
+
+    public int getSpacesMoved() {
+        return spacesMoved;
+    }
+
+    public void setSpacesMoved(int spacesMoved) {
+        this.spacesMoved = spacesMoved;
+    }
+
+    public int getCoinAmount() {
+        return coinAmount;
+    }
+
+    public void setCoinAmount(int coinAmount) {
+        this.coinAmount = coinAmount;
+    }
+
+    public int getStarAmount() {
+        return starAmount;
+    }
+
+    public void setStarAmount(int starAmount) {
+        this.starAmount = starAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "GameState{" +
+                "spacesMoved=" + spacesMoved +
+                ", coinAmount=" + coinAmount +
+                ", starAmount=" + starAmount +
+                '}';
+    }
+}
